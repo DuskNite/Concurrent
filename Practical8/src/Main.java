49dec95128c6d6953dbb498563542169f644b1c6
DuskNite <duskcleric@yahoo.com>
2017-05-30 07:26:51 +0800

    Practical 8 Part 3
    Is the count not needed? If it isnt, the code would definitely be far more cleaner than it is. 
    
    Using an ArrayList, 2 of them, actually, i stored a list of files and a list of hashmaps. Each file has an hashmap associated to it, albeit a little rough. It might be better if i created an object for each of them, which contains a file and a hashmap for a cleaner result. Regardless, it works for now. 
    
    I've added a hmTotal but it seems like it isnt needed. Still, it can be used as a contingency?
    
    The printing part in the end is definitely not done, however, and would need some work.

--- Practical8/src/Main.java
+++ Practical8/src/Main.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,25 +8,73 @@ import java.util.Map;
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        //Define needed variables here.
         HashMap<String,Integer> hm = new HashMap<>();
-        String st = "I have a red cat I like blue cat cats are nice";
-        String[] split = st.split(" ");
+        HashMap<String,Integer> hmTotal = new HashMap<>();
+        ArrayList<HashMap<String,Integer>> hmlist = new ArrayList<>();
+        ArrayList<FileReader> frlist = new ArrayList<>();
 
-        for(String s:split){
-            if(hm.containsKey(s)){
-                Integer j = hm.get(s)+1;
-                hm.put(s,j);
+
+        //Read the 10 files here.
+        frlist.add(new FileReader("src/abstract1.txt"));
+        frlist.add(new FileReader("src/abstract2.txt"));
+        frlist.add(new FileReader("src/abstract3.txt"));
+        frlist.add(new FileReader("src/abstract4.txt"));
+        frlist.add(new FileReader("src/abstract5.txt"));
+        frlist.add(new FileReader("src/abstract6.txt"));
+        frlist.add(new FileReader("src/abstract7.txt"));
+        frlist.add(new FileReader("src/abstract8.txt"));
+        frlist.add(new FileReader("src/abstract9.txt"));
+        frlist.add(new FileReader("src/abstract10.txt"));
+
+        BufferedReader file;
+
+        //For each file, get a string and split it.
+        for(FileReader fr: frlist){
+            file = new BufferedReader(fr);
+
+            String st = file.readLine();
+            String[] split = st.split("[\\p{Punct}\\s]+");
+
+            //For each string, check both hm and hmTotal for the word, and either create a new object, or if the key already exists, increase the key value by 1.
+            for(String s:split){
+                if(hm.containsKey(s)){
+                    Integer j = hm.get(s)+1;
+                    hm.put(s,j);
+                }
+                else{
+                    hm.put(s,1);
+                }
+
+                if(hmTotal.containsKey(s)){
+                    Integer j = hmTotal.get(s)+1;
+                    hmTotal.put(s,j);
+                }
+                else{
+                    hmTotal.put(s,1);
+                }
             }
-            else{
-                hm.put(s,1);
-            }
+
+            //Now clone the completed hm and clear it. Admittedly, it can be a little cleaner, but it probably doesn't matter that much.
+            //hmTotal is not cleared for future use.
+            hmlist.add((HashMap<String,Integer>)hm.clone());
+            hm.clear();
         }
 
-        for(Map.Entry<String, Integer> i : hm.entrySet()){
-            String s = i.getKey();
-            Integer j = i.getValue();
-            System.out.println(s + ": " + j);
+        //For each hashmap, for each entry in the hashmap...
+        int z = 0;
+        for(HashMap<String,Integer> hash : hmlist) {
+            z++;
+            System.out.println("Patent " + z);
+            System.out.println();
+            for (Map.Entry<String, Integer> i : hash.entrySet()) {
+                String s = i.getKey();
+                Integer j = i.getValue();
+                System.out.println(s + ":" + j);
+            }
+            System.out.println();
+            System.out.println("-------------------------------------");
         }
     }
 }
